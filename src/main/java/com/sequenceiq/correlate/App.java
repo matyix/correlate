b1744119093e2784c8aa3ccd8a2c78702a8c344a
Janos Matyas <janos.matyas@sequenceiq.com>
2014-03-06 18:27:28 +0100

    imitial commit

---
+++ src/main/java/com/sequenceiq/correlate/App.java
@@ -0,0 +1,13 @@
+package com.sequenceiq.correlate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
